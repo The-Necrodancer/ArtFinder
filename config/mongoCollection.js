cdfca7acb04e676ceeb2d2519ec3265e7493294f
Suprith Reddy <64047519+Randomname501@users.noreply.github.com>
2025-04-19 18:01:14 -0400

    Added MongoDb Usage
    Added the config folder from out labs that let us use a MongoDb database to store our data

---
+++ config/mongoCollection.js
@@ -0,0 +1,18 @@
+import { dbConnection } from "./mongoConnection.js";
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+//TODO: Add your collection names here
+export const users = getCollectionFn("users");
+export const commissions = getCollectionFn("commissions");
