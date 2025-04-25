71efd5367b17bbed1c4912c86bade44f0f6d2797
Nicole Young <youngnicole2004@gmail.com>
2025-04-25 19:47:48 -0400

    Add docstrings to all methods in data/users.js

--- data/users.js
+++ data/users.js
@@ -25,6 +25,14 @@ import { users } from '../config/mongoCollection.js';
 
 import {checkString, checkStringNaN, validateEmail, throwWrongTypeError, checkId} from '../helpers.js'; 
 
+/**
+ * Adds a user to the database. 
+ * @param {string} role The role of the user, either 'user', 'admin', or 'artist'.
+ * @param {string} username The username of the user.
+ * @param {stirng} email The email of the user (must be in valid email format).
+ * @param {string} password The HASHED password of the user.
+ * @returns {obj} The user that was added. 
+ */
 export const createUser = async ( 
     role,  
     username,
@@ -76,6 +84,11 @@ export const createUser = async (
     return await getUserById(insertedUser.insertedId.toString()); 
 }
 
+/**
+ * Gets user with given id. 
+ * @param {string} id Id of user to find (must be valid id of existing user).
+ * @returns {obj} The user that has given id.
+ */
 export const getUserById = async (id) => {
     id = checkId(id); 
     const userCollection = await users(); 
@@ -85,7 +98,10 @@ export const getUserById = async (id) => {
     return user; 
 }; 
 
-
+/**
+ * Gets all users within the database. 
+ * @returns An array of all users in the database.
+ */
 export const getAllUsers = async() => {
     const userCollection = await users(); 
     let userList = await userCollection.find({}).toArray(); 
@@ -97,12 +113,22 @@ export const getAllUsers = async() => {
     return userList; 
 };
 
+/**
+ * Finds whether username is in use. 
+ * @param {string} username A username to check the database for.
+ * @returns {boolean} True if username is in the database, false otherwise.
+ */
 export const containsUsername = async(username) => {
     username = checkString(username);
     const userCollection = await users(); 
     return (!(await userCollection.find({username})))? false : true;  
 }
 
+/**
+ * Finds whether email is in use. 
+ * @param {string} email A email to check the database for (must be in valid email format).
+ * @returns {bool} True if email is in the database, false otherwise. 
+ */
 export const containsEmail = async(email) => {
     email = checkString(email); 
     if(!validateEmail(email)) throw 'Error: ${email} is not a valid email address.'; 
