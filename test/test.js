4e701c406661df2ba7c1b6b440d38170117c197e
Nicole Young <youngnicole2004@gmail.com>
2025-04-25 18:40:56 -0400

    Change testing of createUser to use random data

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 import testUserMethods from "./user.js";
+import { dbConnection, closeConnection } from "../config/mongoConnection.js";
+
+const db = await dbConnection();
+await db.dropDatabase();
 
 await testUserMethods(); 
 
+await closeConnection(); 
+
 console.log("Finished testing!"); 
-
-
--- test/user.js
+++ test/user.js
@@ -1,23 +1,55 @@
 import { createUser, getUserById, getAllUsers } from "../data/users.js";
-import { dbConnection, closeConnection } from "../config/mongoConnection.js";
+
 import bcrypt from 'bcrypt'; 
+import lodash from 'lodash'; 
+import { createRandomUser } from "../helpers.js";
 
 const testUserMethods = async () => {
-    const db = await dbConnection();
-    await db.dropDatabase();
-
-    console.log( await getAllUsers()); 
-    let password1 = await bcrypt.hash('password1', 10); 
-    let user1 = await createUser('user', 'meow67', 'meow67@gmail.com',  password1); 
-    console.log(user1); 
-    console.log(await getAllUsers()); 
-    let password2 = await bcrypt.hash('password1', 10); 
-    let user2 = await createUser('artist', 'aaaaa', 'aaaa@gmail.com', password2); 
-    console.log(user2); 
-    console.log(await getAllUsers()); 
-    console.log(await getUserById(user1._id));
-    console.log(await getUserById(user2._id));  
-    await closeConnection();
+    const totalNumUsers = 30; 
+    let userList = await testCreateUser(30); 
 }; 
+
+const testCreateUser = async (totalNumUsers) => {
+    const userList = []; 
+    let hasErrors = false; 
+    for(let i=0; i<totalNumUsers; i++) {
+        let newUser = await createRandomUser();
+        let insertedUser; 
+        try {
+            insertedUser = await createUser(newUser.role, newUser.username, newUser.email, newUser.password); 
+        } catch (e) {
+            console.log("FAILURE IN CREATEUSER"); 
+            console.log("Attempted to insert ", newUser); 
+            hasErrors = true; 
+        }
+        newUser._id = insertedUser._id; 
+        newUser.requestedCommissions = []; 
+        newUser.reviewsGiven = []; 
+        if(newUser.role === 'artist') {
+            newUser.artistProfile = {
+                bio: '',
+                portfolio: [],
+                pricingInfo: {},
+                tags: [],
+                availability: false,
+                tos: '',
+                createdCommissions: [],
+                reviewsReceived: [],
+                rating: 0
+            }; 
+        }
+        if(!lodash.isMatch(newUser, insertedUser)) {
+            hasErrors = true; 
+            console.log("FAILURE IN CREATEUSER"); 
+            console.log("Attempt: ", newUser); 
+            console.log("Result: ", insertedUser);  
+        }
+        userList.push(newUser); 
+    }
+    if(!hasErrors) {
+        console.log("createUser passed all test cases."); 
+    }
+    return userList; 
+}
 
 export default testUserMethods; 
