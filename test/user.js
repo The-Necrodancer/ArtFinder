2bf5a50dde479c73eb9dafd190d6fa81ad18ef82
Nicole Young <youngnicole2004@gmail.com>
2025-04-25 18:43:56 -0400

    Add function to test getAllUsers with random data

--- test/user.js
+++ test/user.js
@@ -6,7 +6,8 @@ import { createRandomUser } from "../helpers.js";
 
 const testUserMethods = async () => {
     const totalNumUsers = 30; 
-    let userList = await testCreateUser(30); 
+    let userList = await testCreateUser(totalNumUsers); 
+    await testGetAllUsers(userList, totalNumUsers); 
 }; 
 
 const testCreateUser = async (totalNumUsers) => {
@@ -50,6 +51,28 @@ const testCreateUser = async (totalNumUsers) => {
         console.log("createUser passed all test cases."); 
     }
     return userList; 
+}
+
+const testGetAllUsers = async (userList, totalNumUsers) => {
+    const returnedList = await getAllUsers(); 
+    let hasErrors=false; 
+    for(let i=0; i<totalNumUsers; i++) {
+        if(!returnedList.find((usr) => {return lodash.isMatch(usr, userList[i]);} )) {
+            console.log("FAILURE IN GETALLUSERS"); 
+            console.log("Missing user: ", userList[i]); 
+            hasErrors=true;
+        }
+    }
+    for(let i=0; i<returnedList.length; i++) {
+        if(!userList.find((usr) => {return lodash.isMatch(usr, returnedList[i]);} )) {
+            console.log("FAILURE IN GETALLUSERS"); 
+            console.log("Extra user: ", usr); 
+            hasErrors=true;
+        }
+    }
+    if(!hasErrors) {
+        console.log("getAllUsers passed test case."); 
+    }
 }
 
 export default testUserMethods; 
