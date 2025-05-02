dca4de93202c0ec8f1190de5f5dc1dbeb63f6ea0
Nicole Young <youngnicole2004@gmail.com>
2025-05-01 20:23:18 -0400

    Add artist testing methods to test script

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
 import testUserMethods from "./user.js";
+import testArtistMethods  from "./artist.js";
+import testCommissionsMethods from "./commission.js";
 import { dbConnection, closeConnection } from "../config/mongoConnection.js";
 
 //create database  
 const db = await dbConnection();
 await db.dropDatabase();
 
-/* add test methods here */ 
+/* add test methods here in order of dependency*/ 
 await testUserMethods(); 
+await testArtistMethods();
+await testCommissionsMethods(); 
 
 //close db connection 
 await closeConnection(); 
