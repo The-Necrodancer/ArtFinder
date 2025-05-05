b8da6a4f79240975cba86c677464638630489c6d
Nicole Young <youngnicole2004@gmail.com>
2025-05-04 20:53:33 -0400

    Create middleware file and logging middleware func

---
+++ middleware.js
@@ -0,0 +1,20 @@
+const addCustomMiddleareFunctions = (app) => {
+    printRouteInfo(app); 
+}
+
+
+const printRouteInfo = (app) => {
+    app.use('/', (req, res, next) => {
+        console.log("Current Timestamp: ", new Date().toUTCString()); 
+        console.log("Request Method: ", req.method); 
+        console.log("Request Path: ", req.path); 
+        if(req.session) {
+             console.log("Role is ", req.ression.user.role); 
+        }
+        else console.log("Not authenticated."); 
+        next(); 
+   });
+}
+
+
+export default addCustomMiddleareFunctions; 
