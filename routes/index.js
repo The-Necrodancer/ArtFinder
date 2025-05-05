011a87438f65f8714a033f3a30026e7373fa586b
Suprith Reddy <64047519+Randomname501@users.noreply.github.com>
2025-05-04 22:41:59 -0400

    Fixed a typo with the link to the register link

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const constructorMethod = (app) => {
   app.get("/", async (req, res) => {
     //const featuredArtists = await db.getFeaturedArtists(); // your DB call
     res.render("home", {
-      pageTitle: 'ArtFinder - Find The Artist For You!', 
-      headerTitle: 'ArtFinder', 
+      pageTitle: "ArtFinder - Find The Artist For You!",
+      headerTitle: "ArtFinder",
       navLink: [
-        {link: "/login", text: 'Log In'}, 
-        {lnk: "/register", text: 'Register'}
-      ]
+        { link: "/login", text: "Log In" },
+        { link: "/register", text: "Register" },
+      ],
     });
   });
 
-  app.use("/register", registerRoutes)
+  app.use("/register", registerRoutes);
 
   app.use("/login", loginRoutes);
 
@@ -23,22 +23,21 @@ const constructorMethod = (app) => {
     const { query, style } = req.query;
     //const artists = await db.searchArtists(query, style);
     res.render("browse", {
-      pageTitle: 'Browse Artists', 
-      headerTitle: 'Browse Artists', 
+      pageTitle: "Browse Artists",
+      headerTitle: "Browse Artists",
       navLink: [
-        {link: '/', text: 'home'}, 
-        {link: '/add', text: 'Add artist'}
-      ]
+        { link: "/", text: "home" },
+        { link: "/add", text: "Add artist" },
+      ],
     });
   });
 
   app.get("/artist/:id", async (req, res) => {
     const artist = await getArtistById(req.params.id); // example DB query()
-    res.render("artistProfile", { 
-      artist 
+    res.render("artistProfile", {
+      artist,
     });
   });
-}; 
+};
 
-
-export default constructorMethod; 
+export default constructorMethod;
