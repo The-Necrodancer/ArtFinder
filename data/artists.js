5ea5e5ee2b26a4775480f11070601260f0e85959
Owen Benji K <aegisshadow97@gmail.com>
2025-05-10 21:22:23 -0400

    Redundant commit. Please ignore.

--- data/artists.js
+++ data/artists.js
@@ -151,71 +151,35 @@ export const getArtistById = async (aid) => {
 /**
  * Modifies the artist's profile in the database
  * @param {String} aid The artist's id
- * @param {Object} newProfile The profile of fields to update.
+ * @param {Object} newProfile The profile of fields to update. 
  * @returns {Object} the updated artist object
  */
-export const updateArtistProfile = async (aid, newProfile) => {
-  aid = checkId(aid);
-  let artist = await getArtistById(aid);
-  //checks that newProfile is an Object
-  if (!newProfile || newProfile.constructor !== Object) {
-    throwWrongTypeError("artist profile", "object", typeof newProfile);
-  }
-  //checks that newProfile is not {}
-  if (!Object.keys(newProfile).length)
-    throw "Error: newProfile cannot be empty.";
-  //checks that newProfile only contains acceptable keys (see artistProfileKeys for lsit)
-  for (const k of Object.keys(newProfile)) {
-    if (!artistProfileKeys.includes(k)) {
-      throw `Error: ${k} is not a valid modifiable artist profile key.`;
+export const updateArtistProfile = async(aid, newProfile) => {
+    aid = checkId(aid); 
+    let artist = await getArtistById(aid); 
+    //checks that newProfile is an Object
+    if (!newProfile || newProfile.constructor !== Object) {
+        throwWrongTypeError('artist profile', 'object', typeof newProfile); 
     }
-  }
-  //object that will eventually contain only validated input
-  let validatedObj = { artistProfile: artist.artistProfile };
-  //checks bio if in newProfile
-  if ("bio" in newProfile) {
-    validatedObj.artistProfile.bio = checkBio(newProfile.bio);
-  }
-  //checks portfolio if in newProfile
-  if ("portfolio" in newProfile) {
-    if (!Array.isArray(newProfile.portfolio))
-      throwWrongTypeError("portfolio", "Array", typeof newProfile.portfolio);
-    //validate images here
-  }
-  //checks pricing info if in newProfile
-  if ("pricingInfo" in newProfile) {
-    //makes sure that field pricingInfo is an Object
-    if (
-      !newProfile.pricingInfo ||
-      newProfile.pricingInfo.constructor !== Object
-    ) {
-      throwWrongTypeError(
-        "pricing info",
-        "Object",
-        typeof newProfile.pricingInfo
-      );
+    //checks that newProfile is not {}
+    if(!Object.keys(newProfile).length) throw 'Error: newProfile cannot be empty.'; 
+    //checks that newProfile only contains acceptable keys (see artistProfileKeys for lsit)
+    for(const k of Object.keys(newProfile)) {
+        if (!artistProfileKeys.includes(k)) {
+            throw `Error: ${k} is not a valid modifiable artist profile key.`; 
+        }
     }
-    //sets pricingInfo to empty to later add validated items and prices
-    validatedObj.artistProfile.pricingInfo = {};
-    //for every item and pricing given to this function,
-    //validates the input and then adds it to pricingInfo
-    for (let [key, value] of Object.entries(newProfile.pricingInfo)) {
-      //check that didn't submit duplicate keys:
-      if (Object.keys(validatedObj.artistProfile.pricingInfo).includes(key)) {
-        throw `Error: cannot have duplicate keys.`;
-      }
-      key = checkPricingInfoItem(key);
-      value = checkPriceValue(value);
-      validatedObj.artistProfile.pricingInfo[key] = value;
+    //object that will eventually contain only validated input 
+    let validatedObj = {artistProfile: artist.artistProfile}; 
+    //checks bio if in newProfile 
+    if('bio' in newProfile) {
+        validatedObj.artistProfile.bio = checkBio(newProfile.bio); 
     }
-  }
-  //checks tags if in newProfile
-  if ("tags" in newProfile) {
-    //makes sure given tags is correctly an array
-    if (!Array.isArray(newProfile.tags)) {
-      throwWrongTypeError("tag list", "Array", typeof tags);
+    //checks portfolio if in newProfile 
+    if('portfolio' in newProfile) {
+        if(!Array.isArray(newProfile.portfolio)) throwWrongTypeError('portfolio', 'Array', typeof newProfile.portfolio); 
+        //validate images here 
     }
-<<<<<<< HEAD
     //checks pricing info if in newProfile 
     if('pricingInfo' in newProfile) {
         //makes sure that field pricingInfo is an Object
@@ -228,49 +192,49 @@ export const updateArtistProfile = async (aid, newProfile) => {
         //validates the input and then adds it to pricingInfo
         for (let [key, value] of Object.entries(newProfile.pricingInfo)) {
             //check that didn't submit duplicate keys: 
-            //if(Object.keys(validatedObj.artistProfile.pricingInfo).contains(key)) {
-            if (Object.keys(validatedObj.artistProfile.pricingInfo).includes(key)) {
-                throw `Error: cannot have duplicate keys.`;
+            if(Object.keys(validatedObj.artistProfile.pricingInfo).contains(key)) {
+                throw `Error: cannot have duplicate keys.`; 
             }
             key = checkPricingInfoItem(key);  
             value = checkPriceValue(value); 
             validatedObj.artistProfile.pricingInfo[key] = value; 
         }
-=======
-    validatedObj.artistProfile.tags = [];
-    //validates every tag given
-    for (const t of newProfile.tags) {
-      validatedObj.artistProfile.tags.push(checkTag(t));
->>>>>>> 7aa9146e55ffd2a5ad1233b91996883784987e10
     }
-  }
-  //checks availability if in newProfile
-  if ("availability" in newProfile) {
-    if (typeof newProfile.availability !== "boolean") {
-      throwWrongTypeError(
-        "availability",
-        "boolean",
-        typeof newProfile.availability
-      );
+    //checks tags if in newProfile 
+    if('tags' in newProfile) {
+        //makes sure given tags is correctly an array 
+        if(!Array.isArray(newProfile.tags)) {
+            throwWrongTypeError('tag list', 'Array', typeof tags); 
+        }
+        validatedObj.artistProfile.tags = []; 
+        //validates every tag given 
+        for(const t of newProfile.tags) {
+            validatedObj.artistProfile.tags.push(checkTag(t)); 
+        }
     }
-    validatedObj.artistProfile.availability = newProfile.availability;
-  }
-  //checks tos if in newProfile
-  if ("tos" in newProfile) {
-    if (typeof newProfile.tos !== "string") {
-      throwWrongTypeError("TOS", "string", typeof newProfile.tos);
+    //checks availability if in newProfile 
+    if('availability' in newProfile) {
+        if(typeof newProfile.availability !== 'boolean') {
+            throwWrongTypeError('availability', 'boolean', typeof newProfile.availability ); 
+        }
+        validatedObj.artistProfile.availability = newProfile.availability; 
     }
-    validatedObj.artistProfile.tos = checkTos(newProfile.tos);
-  }
-  //updates artist
-  let userCollection = await users();
-  const updatedArtist = await userCollection.findOneAndUpdate(
-    { _id: new ObjectId(aid) },
-    { $set: validatedObj },
-    { returnDocument: "after" }
-  );
-  if (!updatedArtist) throw "Error: could not update artist in database.";
-  updatedArtist._id = updatedArtist._id.toString();
-  delete updatedArtist.password;
-  return updatedArtist;
+    //checks tos if in newProfile 
+    if('tos' in newProfile) {
+        if(typeof newProfile.tos !== 'string') {
+            throwWrongTypeError('TOS', 'string', typeof newProfile.tos); 
+        }
+        validatedObj.artistProfile.tos = checkTos(newProfile.tos);  
+    }
+    //updates artist 
+    let userCollection = await users(); 
+    const updatedArtist = await userCollection.findOneAndUpdate(
+        {_id: new ObjectId(aid)}, 
+        {$set: validatedObj},
+        {returnDocument: "after"}
+    ); 
+    if(!updatedArtist) throw 'Error: could not update artist in database.'; 
+    updatedArtist._id = updatedArtist._id.toString(); 
+    delete updatedArtist.password;
+    return updatedArtist; 
 };
