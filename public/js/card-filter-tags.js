cde9087f33b1fbed704d19281d7788d2ea35f446
Nicole Young <youngnicole2004@gmail.com>
2025-05-14 15:25:32 -0400

    Create script for tag options

---
+++ public/js/card-filter-tags.js
@@ -0,0 +1,7 @@
+//https://stackoverflow.com/questions/30190588/html-select-multiple-as-dropdown
+
+$(function(){
+    $(".chosen-select").chosen({
+        no_results_text: "Oops, nothing found!"
+    })
+})
